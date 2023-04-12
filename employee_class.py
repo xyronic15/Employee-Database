2ab7b88c732cf173445bccbdd0c4b7a8928a4e9d
gabe-fabia <52291257+gabe-fabia@users.noreply.github.com>
2023-04-12 11:28:12 -0400

    Create employee_class.py
    basline class definition

---
+++ employee_class.py
@@ -0,0 +1,21 @@
+class employee:
+
+    def __init__(self, emp_id, first_name, last_name, emp_date, salary, dept):
+        self.emp_id = emp_id, # int, every employee must have a unique id
+        self.first_name = first_name, # str
+        self.last_name = last_name, # str
+        self.emp_date = emp_date, # datetime obj
+        self.salary = salary, # int
+        self.dept = dept # str
+
+    def __add__():
+        # adds employee, assigns unique id num
+        pass
+
+    def __remove__():
+        # asks for id num, removes emp from database
+        pass
+
+    def __update__():
+        # asks for id num, and which info they want to update
+        pass
