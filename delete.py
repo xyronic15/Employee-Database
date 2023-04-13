8ba98240d960954c4fbe14d84fa8641dc7602e69
sumanavardhan <129995937+sumanavardhan@users.noreply.github.com>
2023-04-13 12:59:12 -0400

    Update delete.py

--- delete.py
+++ delete.py
@@ -1,7 +1,16 @@
-#Delete Employee
-def deleteemp ():  
+#Delete Employee info from Employee Details
+def deleteEmp ():  
 emp_id =int(input("Enter the Employee ID:"))
 if int(int(empd_id)) not in emp_dict.keys():
     print("Entry not found")
 elif:
     print("Employee has been deleted successfully")
+    
+#Delete Employee info from Department Details
+def remove_employee(self, emp_id):
+        for employee in self.employees:
+            if employee.emp_id == emp_id:
+                self.employees.remove(employee)
+                employee.department = None
+                return True
+        return False
